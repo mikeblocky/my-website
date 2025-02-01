acc12cf577cc6f45cb21efe189518b25328deb0b
Minh Nhat Le <mibeblocky@gmail.com>
2025-02-01 22:34:06 +0900

    minor fixes

--- src/app/blog/_data/posts.ts
+++ src/app/blog/_data/posts.ts
@@ -6,7 +6,7 @@ export const blogPosts: BlogPost[] = [
         title: "Misaki Takamatsu speaks about her long-term serialization journey in Skip and Loafer (Translation)",
         description: "A translation of the space that Takamatsu-sensei did on an interview, which released on January 2025.",
         date: "January 31, 2025",
-        readingTime: "15 min read",
+        readingTime: "30 min read",
         slug: "takamatsu-interview-translation-1"
     },
     {
@@ -14,7 +14,7 @@ export const blogPosts: BlogPost[] = [
         title: "Misaki Takamatsu's January 13th Space - translation",
         description: "A translation of the space that Takamatsu-sensei did on January 13th.",
         date: "January 31, 2025",
-        readingTime: "15 min read",
+        readingTime: "45 min read",
         slug: "takamatsu-space-translation"
     },
     {
