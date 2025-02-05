cb4b5c83cd03031f235c5772d80efb88114f25ad
Minh Nhat Le <mibeblocky@gmail.com>
2025-02-05 13:33:59 +0900

    typo fix

--- src/app/blog/_data/posts.ts
+++ src/app/blog/_data/posts.ts
@@ -20,7 +20,7 @@ export const blogPosts: BlogPost[] = [
     {
         id: 2,
         title: "Discord archive - About Shima, Mitsumi, Nao; the seashell, sand, and the sea",
-        description: "A thought that I madw when a person asked me to analyze them.",
+        description: "A thought that I made when a person asked me to analyze them.",
         date: "January 31, 2025",
         readingTime: "15 min read",
         slug: "shima-mitsumi-nao-thoughts"
