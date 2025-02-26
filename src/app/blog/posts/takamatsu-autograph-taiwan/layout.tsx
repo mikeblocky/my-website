d59010fb43e699bb31f993e53007d6e96335cbd3
Minh Nhat Le <151120107+MikebIocky@users.noreply.github.com>
2025-02-26 14:04:26 +0700

    fixing link name

--- src/app/blog/posts/takamatsu-autograph-taiwan/layout.tsx
+++ src/app/blog/posts/takamatsu-autograph-taiwan/layout.tsx
@@ -2,17 +2,17 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Autograph session of Takamatsu-sensei in Taiwan - translation | mikeblocky.com',
-    description: 'About the autograph session that she was invited in two days: February 9th - February 10th.',
+    description: 'About the autograph session which she was invited in two days: February 9th - February 10th.',
     openGraph: {
         title: 'Autograph session of Takamatsu-sensei in Taiwan - translation| mikeblocky.com',
-        description: 'About the autograph session that she was invited  in two days: February 9th - February 10th.',
+        description: 'About the autograph session which she was invited  in two days: February 9th - February 10th.',
         type: 'article',
         publishedTime: '2025-02-26T00:00:00.000Z',
     },
     twitter: {
         card: 'summary_large_image',
         title: 'Autograph session of Takamatsu-sensei in Taiwan - translation',
-        description: 'About the autograph session that she was invited on two days: February 9th - February 10th.',
+        description: 'About the autograph session which she was invited on two days: February 9th - February 10th.',
     }
 }
 
