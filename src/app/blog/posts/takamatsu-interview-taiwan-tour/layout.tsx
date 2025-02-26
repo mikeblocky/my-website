300e838dc99b9aee7cead07711eabb12fd386907
Minh Nhat Le <151120107+MikebIocky@users.noreply.github.com>
2025-02-26 13:59:13 +0700

    fixing grammar

--- src/app/blog/posts/takamatsu-interview-taiwan-tour/layout.tsx
+++ src/app/blog/posts/takamatsu-interview-taiwan-tour/layout.tsx
@@ -2,17 +2,17 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Autograph session of Takamatsu-sensei in Taiwan - translation | mikeblocky.com',
-    description: 'About the autograph session that she went to on February 25th.',
+    description: 'About the autograph session that she participated in two days: February 9th - February 10th.',
     openGraph: {
         title: 'Autograph session of Takamatsu-sensei in Taiwan - translation| mikeblocky.com',
-        description: 'About the autograph session that she went to on February 25th.',
+        description: 'About the autograph session that she participated in two days: February 9th - February 10th.',
         type: 'article',
         publishedTime: '2025-02-26T00:00:00.000Z',
     },
     twitter: {
         card: 'summary_large_image',
         title: 'Autograph session of Takamatsu-sensei in Taiwan - translation',
-        description: 'About the autograph session that she went to on February 25th.',
+        description: 'About the autograph session that she participated on two days: February 9th - February 10th.',
     }
 }
 
