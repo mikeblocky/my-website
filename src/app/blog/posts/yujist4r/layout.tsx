229cd04e46c8eb1306833a27e10c43e6213efea1
Minh Nhat Le <151120107+MikebIocky@users.noreply.github.com>
2025-03-03 19:17:46 +0700

    secret posts

---
+++ src/app/blog/posts/yujist4r/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Autograph session of Takamatsu-sensei in Taiwan - translation | mikeblocky.com',
+    description: 'About the autograph session which she was invited in two days: February 9th - February 10th.',
+    openGraph: {
+        title: 'Autograph session of Takamatsu-sensei in Taiwan - translation| mikeblocky.com',
+        description: 'About the autograph session which she was invited  in two days: February 9th - February 10th.',
+        type: 'article',
+        publishedTime: '2025-02-26T00:00:00.000Z',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Autograph session of Takamatsu-sensei in Taiwan - translation',
+        description: 'About the autograph session which she was invited on two days: February 9th - February 10th.',
+    }
+}
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return children
+} 
